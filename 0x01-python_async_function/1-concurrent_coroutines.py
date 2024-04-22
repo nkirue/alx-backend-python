6f8ded4abd42bd8fc1d52ec3064c678dab0fd2de
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2024-04-22 14:00:44 +0000

    Python - Async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    
     wait_times = await asyncio.gather(
         *tuple(map(lambda _: wait_random(max_delay), range(n)))
     )
